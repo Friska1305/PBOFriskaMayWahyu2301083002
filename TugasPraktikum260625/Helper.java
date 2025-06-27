0c70e9cc2d1d62cab51d56ed7084009b32070b97
Friska1305 <161124139+Friska1305@users.noreply.github.com>
2025-06-27 20:52:07 +0700

    Add files via upload

---
+++ TugasPraktikum260625/Helper.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPraktikum260625;
+
+/**
+ *
+ * @author frisk
+ */
+public class Helper {
+    public static int saldo = 1000000; // saldo awal
+    
+}
